5b956bb00330c126d8311e6f7f96fc33b2106c51
nb90785 <nb90785@navercorp.com>
2023-05-19 17:18:24 +0900

    first commit

---
+++ sanity/schemas/post.ts
@@ -0,0 +1,12 @@
+export default {
+  title: "Post",
+  name: "post",
+  type: "document",
+  fields: [
+    { title: "Username", name: "username", type: "string" },
+    { title: "Username", name: "username", type: "string" },
+    { title: "Name", name: "name", type: "string" },
+    { title: "Email", name: "email", type: "string" },
+    { title: "Image", name: "image", type: "string" },
+  ],
+};
